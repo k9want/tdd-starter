2e5e7d43e9f45196a9ce5d85d710bbde63157af2
Sangkwon Lee <sangkwon26@gmail.com>
2024-05-14 18:48:17 +0900

    Test: 테스트 설정 수정

--- src/test/java/com/fastcamp/tddstarter/TddStarterApplicationTests.java
+++ src/test/java/com/fastcamp/tddstarter/TddStarterApplicationTests.java
@@ -1,9 +1,13 @@
 package com.fastcamp.tddstarter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.ANY, connection = EmbeddedDatabaseConnection.H2)
 class TddStarterApplicationTests {
 
     @Test
